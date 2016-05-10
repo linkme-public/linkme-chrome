ceb4f75c5c3a79d00840a8173dfa2ae34632de78
Olivia Ifrim <olly2204@yahoo.com>
2016-05-10 23:47:47 +0200

    Adding popup message after posting is completed.

--- popup.js
+++ popup.js
@@ -1,18 +1,39 @@
+function updatePopup(status, link) {
+  document.getElementById('status').innerHTML = status;
+  document.getElementById('currentLink').innerHTML = link;
+}
+
 chrome.tabs.query({
-    'active': true, 
-    'windowId':chrome.windows.WINDOW_ID_CURRENT},
-    
-    function(tabs){
-      document.getElementById('currentLink').innerHTML = tabs[0].url;
-      extractedLink = tabs[0].url;
-    
-    var params = { "link": tabs[0].url }
+  'active': true,
+  'windowId': chrome.windows.WINDOW_ID_CURRENT
+},
+
+  function (tabs) {
+
+    // Get the link
+    var extractedLink = tabs[0].url;
+
+    // Update the popup
+    updatePopup("Posting ...", extractedLink);
+
+    // Post to API
+    var params = { "link": extractedLink }
 
     var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState == 4) {
+        if (xhr.status == 200) {
+          updatePopup("Link posted successfully!", "");
+        } else {
+          updatePopup("Something went wrong, try again!", "");
+        }
+      }
+    }
+
     xhr.open("POST", "http://linkme-web.azurewebsites.net/api/link", true);
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhr.send(JSON.stringify(params)); 
-});
+    xhr.send(JSON.stringify(params));
+  });
 
 
 
